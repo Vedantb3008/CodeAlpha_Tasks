afbccdccd89bda73fb63e34ca0322a73b3d53732
Vedant Borkar <vedantborkar0630@gmail.com>
2026-02-11 11:25:50 +0530

    new portfolio add

---
+++ script.js
@@ -0,0 +1,26 @@
+const text = ["Frontend Developer", "Web Designer", "JavaScript Learner"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+function type(){
+if(count === text.length){
+count = 0;
+}
+
+currentText = text[count];
+letter = currentText.slice(0, ++index);
+
+document.getElementById("typing").textContent = letter;
+
+if(letter.length === currentText.length){
+count++;
+index = 0;
+setTimeout(type,1000);
+}else{
+setTimeout(type,80);
+}
+}
+
+type();
