db25ab09b9a0aaad2ab53d3cea90062a658689af
Vedant Borkar <vedantborkar0630@gmail.com>
2026-02-12 20:35:12 +0530

    CODE FIXED

--- script.js
+++ script.js
@@ -1,26 +1,2 @@
-const text = ["Frontend Developer", "Web Designer", "JavaScript Learner"];
-let count = 0;
-let index = 0;
-let currentText = "";
-let letter = "";
-
-function type(){
-if(count === text.length){
-count = 0;
-}
-
-currentText = text[count];
-letter = currentText.slice(0, ++index);
-
-document.getElementById("typing").textContent = letter;
-
-if(letter.length === currentText.length){
-count++;
-index = 0;
-setTimeout(type,1000);
-}else{
-setTimeout(type,80);
-}
-}
-
-type();
+// future animations ya effects ke liye empty file
+console.log("Portfolio Loaded");
